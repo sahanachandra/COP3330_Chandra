7ed3f0b79093750bd347ca43522c7cde4067e3cc
sahanachandra <71294503+sahanachandra@users.noreply.github.com>
2020-10-12 19:46:16 -0400

    Update Pyramid.java

--- chandra_p3/src/Pyramid.java
+++ chandra_p3/src/Pyramid.java
@@ -1,24 +1,26 @@
 public class Pyramid extends Shape3D {
-
-    public Pyramid(double length, double width, double height){
-        this.length = length;
-        this.width = width;
-        this.height = height;
+    private double length;
+    private double width;
+    private double height;
+    public Pyramid(double length, double width, double height) {
+        super();
+        this.leng = length;
+        this.wid = width;
+        this.hei = height;
     }
-
     @Override
-    String getName() {
+    public String getName() {
         return "pyramid";
     }
 
     @Override
-    double getArea() {
-        return (length * width) + (length * Math.sqrt(Math.pow(width / 2.0, 2) +
-                Math.pow(height, 2))) + (width * Math.sqrt(Math.pow(length / 2.0, 2) + Math.pow(height, 2)));
+    public double getArea() {
+        return (leng * wid) + (leng * Math.sqrt(Math.pow(wid / 2.0, 2) +
+                Math.pow(hei, 2))) + (wid * Math.sqrt(Math.pow(leng / 2.0, 2) + Math.pow(hei, 2)));
     }
 
     @Override
-    double getVolume() {
-        return (length*width*height)/(3.0);
+    public double getVolume() {
+        return (leng*wid*hei)/(3.0);
     }
 }
